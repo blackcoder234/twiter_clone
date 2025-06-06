dc2f84989cebff612d0e0d4dc20b7e50d671b7d6
shadows237 <165073025+blackcoder234@users.noreply.github.com>
2025-06-06 19:45:22 +0530

    Update Django settings for production deployment
    - Set DEBUG to False for production environment.
    - Updated ALLOWED_HOSTS to include '.onrender.com'.
    - Changed database configuration from SQLite to PostgreSQL with appropriate credentials.
    - Added WhiteNoise middleware for serving static files.
    - Configured STATIC_ROOT and STATICFILES_STORAGE for static file management.
    - Added requirements.txt with necessary dependencies for the project.

--- twitter_project/twitter_project/settings.py
+++ twitter_project/twitter_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c17!o2sd5a$ogt$u)$lfi0qhr0^=n@y4ilw$p4&r#&6pul5w#f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'twitter_project.urls'
@@ -85,8 +86,12 @@ WSGI_APPLICATION = 'twitter_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'twitter_clone_db_gk86',
+        'USER': 'twitter_clone_db_gk86_user',
+        'PASSWORD': 'SKmcARLMMTOXptCUNAvjdq4liHUOsxpS',
+        'HOST': 'dpg-d11fb1ndiees73fagj2g-a',
+        'PORT': '5432',
     }
 }
 
@@ -124,7 +129,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
 
