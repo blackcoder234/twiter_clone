a735b5688ee78b98f6dd88d06e7f229a67361dfb
shadows237 <165073025+blackcoder234@users.noreply.github.com>
2025-06-07 09:35:33 +0530

    Add folder parameter to CloudinaryField for user avatars and tweet photos

--- twitter_project/twitter_app/models.py
+++ twitter_project/twitter_app/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(max_length=200, blank=True)
-    avatar = CloudinaryField("avatar", blank=True, null=True)
+    avatar = CloudinaryField("avatar", folder="user_avatars", blank=True, null=True)
 
     def __str__(self):
         return self.user.username
@@ -13,7 +13,7 @@ class UserProfile(models.Model):
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tweets")
     text = models.TextField(max_length=280)
-    photo = CloudinaryField("photo",  blank=True, null=True)
+    photo = CloudinaryField("photo", folder="tweet_photos", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
